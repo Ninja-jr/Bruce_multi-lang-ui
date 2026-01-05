72ceceb6bb962c4f8e4cddf966700abd39f29062
Ninja-jr <ninjajr1983@gmail.com>
2026-01-05 14:32:49 +0100

    Create apple_spam.h

---
+++ src/modules/ble/apple_spam.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Arduino.h>
+#include <NimBLEAdvertisedDevice.h>
+#include <NimBLEDevice.h>
+#include <NimBLEServer.h>
+
+struct ApplePayload {
+    const char* name;
+    const uint8_t* data;
+    uint8_t length;
+};
+
+void appleSubMenu();
+void startAppleSpam(int payloadIndex);
+void stopAppleSpam();
+bool isAppleSpamRunning();
+const char* getApplePayloadName(int index);
+int getApplePayloadCount();
