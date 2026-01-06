fc0892a573cf4b1cc030bb41280d3685092bfafb
Ninja-jr <ninjajr1983@gmail.com>
2026-01-06 19:28:36 +0100

    Update ble_spam.cpp reversing mac randomization changes. Didn't work

--- src/modules/ble/ble_spam.cpp
+++ src/modules/ble/ble_spam.cpp
@@ -355,16 +355,11 @@ void executeSpam(EBLEPayloadType type) {
         return;
     }
     
-#if defined(CONFIG_IDF_TARGET_ESP32C5)
-    esp_bt_controller_deinit();
-#else
-    BLEDevice::deinit(false);
-#endif
-    vTaskDelay(10 / portTICK_PERIOD_MS);
-    
+    uint8_t macAddr[6];
+    generateRandomMac(macAddr);
+    esp_base_mac_addr_set(macAddr);
+
     BLEDevice::init("");
-    BLEDevice::setOwnAddrType(BLE_OWN_ADDR_RANDOM);
-    
     vTaskDelay(5 / portTICK_PERIOD_MS);
     esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, MAX_TX_POWER);
     pAdvertising = BLEDevice::getAdvertising();
@@ -382,24 +377,20 @@ void executeSpam(EBLEPayloadType type) {
 
     pAdvertising->stop();
     vTaskDelay(5 / portTICK_PERIOD_MS);
-    
 #if defined(CONFIG_IDF_TARGET_ESP32C5)
     esp_bt_controller_deinit();
 #else
-    BLEDevice::deinit(false);
+    BLEDevice::deinit();
 #endif
 }
 
 void executeCustomSpam(String spamName) {
-#if defined(CONFIG_IDF_TARGET_ESP32C5)
-    esp_bt_controller_deinit();
-#else
-    BLEDevice::deinit(false);
-#endif
-    vTaskDelay(10 / portTICK_PERIOD_MS);
-    
+    uint8_t macAddr[6];
+    for (int i = 0; i < 6; i++) { macAddr[i] = esp_random() & 0xFF; }
+    macAddr[0] = (macAddr[0] | 0xF0) & 0xFE;
+    esp_base_mac_addr_set(macAddr);
+
     BLEDevice::init("sh4rk");
-    BLEDevice::setOwnAddrType(BLE_OWN_ADDR_RANDOM);
 
     vTaskDelay(5 / portTICK_PERIOD_MS);
 
@@ -423,17 +414,15 @@ void executeCustomSpam(String spamName) {
 
     pAdvertising->stop();
     vTaskDelay(10 / portTICK_PERIOD_MS);
-    
 #if defined(CONFIG_IDF_TARGET_ESP32C5)
     esp_bt_controller_deinit();
 #else
-    BLEDevice::deinit(false);
+    BLEDevice::deinit();
 #endif
 }
 
 void ibeacon(const char *DeviceName, const char *BEACON_UUID, int ManufacturerId) {
     BLEDevice::init(DeviceName);
-    BLEDevice::setOwnAddrType(BLE_OWN_ADDR_RANDOM);
 
     vTaskDelay(5 / portTICK_PERIOD_MS);
 
@@ -477,7 +466,7 @@ void ibeacon(const char *DeviceName, const char *BEACON_UUID, int ManufacturerId
 #if defined(CONFIG_IDF_TARGET_ESP32C5)
     esp_bt_controller_deinit();
 #else
-    BLEDevice::deinit(false);
+    BLEDevice::deinit();
 #endif
 }
 
@@ -538,10 +527,14 @@ void aj_adv(int ble_choice) {
             }
         }
         
+        BLEDevice::init("");
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        pAdvertising = nullptr;
+        vTaskDelay(100 / portTICK_PERIOD_MS);
 #if defined(CONFIG_IDF_TARGET_ESP32C5)
         esp_bt_controller_deinit();
 #else
-        BLEDevice::deinit(false);
+        BLEDevice::deinit();
 #endif
         return;
     }
@@ -578,9 +571,13 @@ void aj_adv(int ble_choice) {
         }
     }
 
+    BLEDevice::init("");
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    pAdvertising = nullptr;
+    vTaskDelay(100 / portTICK_PERIOD_MS);
 #if defined(CONFIG_IDF_TARGET_ESP32C5)
     esp_bt_controller_deinit();
 #else
-    BLEDevice::deinit(false);
+    BLEDevice::deinit();
 #endif
 }
