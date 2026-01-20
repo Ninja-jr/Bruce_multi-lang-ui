709dc9ebcbb99ef54f5859312968abb71eccc323
Ninja-jr <ninjajr1983@gmail.com>
2026-01-20 03:02:02 +0100

    Update whisperpair.h

--- src/modules/ble/whisperpair.h
+++ src/modules/ble/whisperpair.h
@@ -6,3 +6,5 @@ bool requireButtonHoldConfirmation(const char* message, uint32_t ms = 3000);
 void testFastPairVulnerability();
 bool attemptKeyBasedPairing(NimBLEAddress target);
 void whisperPairMenu();
+void whisperPairFullBenchmark();
+bool whisperPairFullExploit(NimBLEAddress target);
