e5ad0dbebb550bb3ad06404017a9c529225408a3
Ninja-jr <ninjajr1983@gmail.com>
2026-01-20 02:18:42 +0100

    Create whisperpair.h

---
+++ src/modules/ble/whisperpair.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <Arduino.h>
+#include <NimBLEDevice.h>
+
+bool requireButtonHoldConfirmation(const char* message, uint32_t ms = 3000);
+void testFastPairVulnerability();
+bool attemptKeyBasedPairing(NimBLEAddress target);
+void whisperPairMenu();
