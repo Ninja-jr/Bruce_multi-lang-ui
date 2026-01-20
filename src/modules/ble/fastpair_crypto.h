80bdfeb74962910329f863e2c6d4b430371c8303
Ninja-jr <ninjajr1983@gmail.com>
2026-01-20 06:25:39 +0100

    Update fastpair_crypto.h

--- src/modules/ble/fastpair_crypto.h
+++ src/modules/ble/fastpair_crypto.h
@@ -11,24 +11,29 @@ private:
     mbedtls_aes_context aes_ctx;
     mbedtls_ctr_drbg_context ctr_drbg;
     mbedtls_entropy_context entropy;
+
+    mbedtls_ecp_group ecdh_grp;
+    mbedtls_mpi ecdh_d;
+    mbedtls_ecp_point ecdh_Q;
+    mbedtls_mpi ecdh_z;
     
     uint8_t shared_secret[32];
     uint8_t account_key[16];
-    
+
 public:
     FastPairCrypto();
     ~FastPairCrypto();
-    
+
     bool generateKeyPair(uint8_t* public_key, size_t* pub_len);
     bool computeSharedSecret(const uint8_t* peer_public, size_t peer_len);
     bool deriveFastPairKeys(const uint8_t* nonce, size_t nonce_len);
-    
+
     void encryptCTR(uint8_t* data, size_t len, const uint8_t* key, const uint8_t* nonce);
     void decryptCTR(uint8_t* data, size_t len, const uint8_t* key, const uint8_t* nonce);
-    
+
     void generateAccountKey();
     const uint8_t* getAccountKey() { return account_key; }
-    const uint8_t* getSharedSecret() { return shared_secret; }
-    
+    const uint8_t* getSharedSecret();
+
     void benchmark();
 };
