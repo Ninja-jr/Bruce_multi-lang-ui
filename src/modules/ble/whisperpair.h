4ce362921e42705134569ed9d1253606f99daef5
Ninja-jr <ninjajr1983@gmail.com>
2026-01-21 23:59:58 +0100

    Update whisperpair.h

--- src/modules/ble/whisperpair.h
+++ src/modules/ble/whisperpair.h
@@ -4,16 +4,13 @@
 #include <MenuItemInterface.h>
 #include <functional>
 #include <vector>
-
 extern int loopOptions(std::vector<Option>& options, uint8_t type, const char* title, int index, bool interpreter);
-
 bool requireSimpleConfirmation(const char* message);
 void testFastPairVulnerability();
 bool attemptKeyBasedPairing(NimBLEAddress target);
 String selectTargetFromScan(const char* title);
 void whisperPairMenu();
-
 bool fastpair_ecdh_key_exchange(NimBLEAddress target, uint8_t* shared_secret);
 bool fastpair_complete_pairing(NimBLEAddress target, const uint8_t* shared_secret);
-void fastpair_benchmark();
 bool whisperPairFullExploit(NimBLEAddress target);
+void updateScanDisplay(uint32_t foundCount, uint32_t elapsedMs, bool forceRedraw = false);
