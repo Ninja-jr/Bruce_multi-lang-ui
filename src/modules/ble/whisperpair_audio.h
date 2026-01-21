f8f46ce08aca367f8b581fef8b618d452209b24c
Ninja-jr <ninjajr1983@gmail.com>
2026-01-21 01:53:50 +0100

    Update whisperpair_audio.h

--- src/modules/ble/whisperpair_audio.h
+++ src/modules/ble/whisperpair_audio.h
@@ -2,8 +2,6 @@
 #include <NimBLEDevice.h>
 #include <Arduino.h>
 
-bool requireButtonHoldConfirmation(const char* message, uint32_t ms = 3000);
-
 class AudioCommandService {
 private:
     NimBLEServer* pServer;
@@ -11,11 +9,11 @@ private:
     NimBLECharacteristic* pAudioCmdChar;
     NimBLECharacteristic* pAudioDataChar;
     bool isRunning = false;
-    
+
     class AudioCmdCallbacks : public NimBLECharacteristicCallbacks {
         void onWrite(NimBLECharacteristic* pCharacteristic);
     };
-    
+
 public:
     void begin();
     void sendAudioCommand(const char* cmd);
