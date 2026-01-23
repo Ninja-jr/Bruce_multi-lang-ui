4843fa4cd40a2eb6b3d5a843d559573e96bd7a51
Ninja-jr <ninjajr1983@gmail.com>
2026-01-23 02:54:48 +0100

    Update whisperpair.h

--- src/modules/ble/whisperpair.h
+++ src/modules/ble/whisperpair.h
@@ -1,17 +1,23 @@
 #pragma once
+
 #include <Arduino.h>
-#include <BLEDevice.h>
+#include <NimBLEBeacon.h>
+#include <NimBLEDevice.h>
+#include <NimBLEServer.h>
+#include <NimBLEUtils.h>
+
 #include <MenuItemInterface.h>
 #include <functional>
 #include <vector>
+
 extern int loopOptions(std::vector<Option>& options, uint8_t type, const char* title, int index, bool interpreter);
 bool initBLEIfNeeded(const char* deviceName = "whisperpair");
 bool requireSimpleConfirmation(const char* message);
 void testFastPairVulnerability();
-bool attemptKeyBasedPairing(BLEAddress target);
+bool attemptKeyBasedPairing(NimBLEAddress target);
 String selectTargetFromScan(const char* title);
 void whisperPairMenu();
-bool fastpair_ecdh_key_exchange(BLEAddress target, uint8_t* shared_secret);
-bool fastpair_complete_pairing(BLEAddress target, const uint8_t* shared_secret);
-bool whisperPairFullExploit(BLEAddress target);
+bool fastpair_ecdh_key_exchange(NimBLEAddress target, uint8_t* shared_secret);
+bool fastpair_complete_pairing(NimBLEAddress target, const uint8_t* shared_secret);
+bool whisperPairFullExploit(NimBLEAddress target);
 void updateScanDisplay(uint32_t foundCount, uint32_t elapsedMs, bool forceRedraw = false);
