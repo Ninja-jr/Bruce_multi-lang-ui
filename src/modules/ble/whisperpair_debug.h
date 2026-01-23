5abc21a93f225293f49f6d4299a2802dab793bb2
Ninja-jr <ninjajr1983@gmail.com>
2026-01-23 03:07:07 +0100

    Delete whisperpair_debug.h

--- src/modules/ble/whisperpair_debug.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-#include <NimBLEDevice.h>
-void printBLEInfo();
-void testBLEScanner();
-void testBLEConnection();
-void memoryCheck();
-void fastpair_benchmark();
-void whisperPairDebugMenu();
