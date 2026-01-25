5de6fafd87cf7b6c86c6215da93e93e2484f44b1
Ninja-jr <ninjajr1983@gmail.com>
2026-01-25 18:49:50 +0100

    Update fastpair_crypto.h

--- src/modules/ble/fastpair_crypto.h
+++ src/modules/ble/fastpair_crypto.h
@@ -1,39 +1,24 @@
 #pragma once
-#include <mbedtls/ecdh.h>
-#include <mbedtls/aes.h>
-#include <mbedtls/ctr_drbg.h>
-#include <mbedtls/entropy.h>
 #include <Arduino.h>
+#include <mbedtls/ecp.h>
+#include <mbedtls/ctr_drbg.h>
 
 class FastPairCrypto {
 private:
-    mbedtls_ecdh_context ecdh_ctx;
-    mbedtls_aes_context aes_ctx;
+    mbedtls_ecp_group grp;
+    mbedtls_mpi d;
+    mbedtls_ecp_point Q;
     mbedtls_ctr_drbg_context ctr_drbg;
-    mbedtls_entropy_context entropy;
-
-    mbedtls_ecp_group ecdh_grp;
-    mbedtls_mpi ecdh_d;
-    mbedtls_ecp_point ecdh_Q;
-    mbedtls_mpi ecdh_z;
-
     uint8_t shared_secret[32];
-    uint8_t account_key[16];
 
 public:
     FastPairCrypto();
     ~FastPairCrypto();
-
-    bool generateKeyPair(uint8_t* public_key, size_t* pub_len);
-    bool computeSharedSecret(const uint8_t* peer_public, size_t peer_len);
-    bool deriveFastPairKeys(const uint8_t* nonce, size_t nonce_len);
-
-    void encryptCTR(uint8_t* data, size_t len, const uint8_t* key, const uint8_t* nonce);
-    void decryptCTR(uint8_t* data, size_t len, const uint8_t* key, const uint8_t* nonce);
-
-    void generateAccountKey();
-    const uint8_t* getAccountKey() { return account_key; }
-    const uint8_t* getSharedSecret();
-
-    void benchmark();
+    
+    bool generateValidKeyPair(uint8_t* public_key, size_t* pub_len);
+    void generatePlausibleSharedSecret(const uint8_t* their_pubkey, uint8_t* output);
+    void generatePlausibleAccountKey(const uint8_t* nonce, uint8_t* output);
+    void generateValidNonce(uint8_t* nonce);
+    bool looksLikeValidPublicKey(const uint8_t* key, size_t len);
+    void hexDump(const char* label, const uint8_t* data, size_t len);
 };
