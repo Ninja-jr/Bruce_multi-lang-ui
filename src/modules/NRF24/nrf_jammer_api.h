d3b3a8d94cb6712c10dedd35e4cf74750a45aa38
Ninja-jr <ninjajr1983@gmail.com>
2026-01-27 15:37:44 +0100

    Create nrf_jammer_api.h to bridge nrf jammer with whisperpair functionality

---
+++ src/modules/NRF24/nrf_jammer_api.h
@@ -0,0 +1,13 @@
+#ifndef __NRF_JAMMER_API_H
+#define __NRF_JAMMER_API_H
+
+#include "modules/NRF24/nrf_common.h"
+
+bool isNRF24Available();
+void startJammer();
+void stopJammer();
+void setJammerMode(int modeIndex);
+int getCurrentJammerMode();
+void updateJammerChannel();
+
+#endif
